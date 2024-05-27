d7e65842b4d34cd675028e4d39fb959160e60ba5
20essentials <s0l0estudi4r@gmail.com>
2024-05-28 00:34:06 +0200

    v0.0.2

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-console.log("Based in ->", "https://www.logorado.com/portfolio/fastfood");
-console.log("Based in ->", "https://youtu.be/lsai17vkkek?si=_jSysZ3a17Xa48CJ");
+console.log("Based in :", "https://www.logorado.com/portfolio/fastfood");
+console.log("Based in :", "https://youtu.be/lsai17vkkek?si=_jSysZ3a17Xa48CJ");
